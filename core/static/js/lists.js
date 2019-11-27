081de2c43fa98973646a35b9531cf87798645c60
Sam van der Kris <samvdkris@gmail.com>
2019-11-27 21:34:23 +0100

    Fix add wordpair function

--- core/static/js/lists.js
+++ core/static/js/lists.js
@@ -1,9 +1,10 @@
 var block = document.getElementsByClassName("block")[0];
-var wordpair = document.getElementsByClassName("wordpair")[0];
+var wordpair = '<div class="wordpair">\n<input type="text" class="from" placeholder="From">\n<input type="text" class="to" placeholder="To" onfocus="checkSelection()">\n</div>';
 function checkSelection() {
     var wordpairs = document.getElementsByClassName("wordpair");
     var lastwordpair = wordpairs[wordpairs.length - 1];
     if (lastwordpair.children[1] === document.activeElement) {
+        block.insertAdjacentHTML("beforeend", wordpair);
     }
 }
 function createList() {
--- core/static/js/lists.ts
+++ core/static/js/lists.ts
@@ -1,16 +1,13 @@
 let block = document.getElementsByClassName("block")[0];
-let wordpair = document.getElementsByClassName("wordpair")[0];
+let wordpair = '<div class="wordpair">\n<input type="text" class="from" placeholder="From">\n<input type="text" class="to" placeholder="To" onfocus="checkSelection()">\n</div>';
 
 
 function checkSelection(){
 	let wordpairs = document.getElementsByClassName("wordpair");
 	let lastwordpair = wordpairs[wordpairs.length-1];
 	if (lastwordpair.children[1] === document.activeElement) {  // Check if the last wordpair is selected
-		// block.insertAdjacentElement("beforeend", wordpair.cloneNode(true));
-		// TODO: add wordpair not working?
+		block.insertAdjacentHTML("beforeend", wordpair);
 	}
-
-
 }
 
 function createList() {
